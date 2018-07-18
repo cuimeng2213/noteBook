810981c590ed84c2c9a0591981473b068c08a879
cuimeng <cuimenggood@126.com>
2018-07-19 00:01:55 +0800

    完成首页分页和搜索下拉框
    完成首页分页和搜索下拉框

--- djangoProject/myBook/managebook/views.py
+++ djangoProject/myBook/managebook/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.core.paginator import Paginator
+from django.db.models import Q
 from .models import *
 
 # Create your views here.
@@ -12,3 +14,24 @@ class bookView(ListView):
 	template_name = 'base.html'
 	model = Book
 	context_object_name = 'book_obj'
+
+	def get_queryset(self):
+		
+		pageNum = self.request.GET.get('pageNum',1)
+		print('########### ',pageNum)
+		queryset = super(bookView, self).get_queryset()
+
+		allBook = self.model.objects.all()
+
+		pager = Paginator(allBook, 1)
+
+		currentPage = pager.page(pageNum)
+		print(currentPage)
+		return currentPage
+
+	def get_context_data(self, **kwargs):
+		context = super(bookView,self).get_context_data(**kwargs)
+		typeBook = TypeBook.objects.all()
+		context['typeBook'] = typeBook
+		print('##########context: ',context)
+		return context
