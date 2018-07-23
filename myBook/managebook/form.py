e89e0ebdd4f01e8124f8056e7bdf84cd7343daf3
cuimeng <cuimenggood@126.com>
2018-07-24 07:35:52 +0800

    修改form表单
    修改图书form表单

--- myBook/managebook/form.py
+++ myBook/managebook/form.py
@@ -2,40 +2,58 @@
 
 from django import forms
 from django.forms import widgets
+from .models import *
 
 '''
-	name = models.CharField(max_length=20, verbose_name='书名')
-	publish_year = models.DateField(verbose_name='出版年份')
-	publish_add = models.DateTimeField(auto_now_add=True, verbose_name='添加时间')
-	price = models.IntegerField(verbose_name='价格')
-	stocks = models.IntegerField(verbose_name='库存')
-	status = models.BooleanField(default=True, verbose_name='出版状态')
-	type = models.ForeignKey('TypeBook')
-	author = models.ManyToManyField('Author')
-	publisher = models.ForeignKey('Publisher')
-	info = models.OneToOneField('Details', blank=True, null=True, unique=True)
+name = models.CharField(max_length=20, verbose_name='书名')
+publish_year = models.DateField(verbose_name='出版年份')
+publish_add = models.DateTimeField(auto_now_add=True, verbose_name='添加时间')
+price = models.IntegerField(verbose_name='价格')
+stocks = models.IntegerField(verbose_name='库存')
+status = models.BooleanField(default=True, verbose_name='出版状态')
+type = models.ForeignKey('TypeBook')
+author = models.ManyToManyField('Author')
+publisher = models.ForeignKey('Publisher')
+info = models.OneToOneField('Details', blank=True, null=True, unique=True)
 '''
 
 class BookForm(forms.Form):
+	'''
+	图书添加表单
+	'''
 	name = forms.CharField(max_length=32, min_length=2, 
-			widget=widgets.TextInput( attrs={'class':'form-control','placeholder':'书名'} )
-			)
+		widget=widgets.TextInput( attrs={'class':'form-control','placeholder':'书名','id':'bookname'} )
+		)
 	publisher_year = forms.DateField(
 		widget = widgets.DateInput(
-			attrs={'class':'form-control','placeholder':'出版年份'}),
-	)
+			attrs={'class':'form-control','placeholder':'出版日期','id':'publisher_year'}),
+		)
 
 	price = forms.IntegerField(
-		widget = widgets.NumberInput(attrs={'class':'form-control','placeholder':'价格'})
-	)
+		widget = widgets.NumberInput(attrs={'class':'form-control','placeholder':'价格','id':'price'})
+		)
 	stocks = forms.IntegerField(
-		widget = widgets.NumberInput(attrs={'class':'form-control','placeholder':'库存'})
-	)
+		widget = widgets.NumberInput(attrs={'class':'form-control','placeholder':'库存','id':'stocks'})
+		)
 	status = forms.ChoiceField(
-		widget=widgets.Select(attrs={'class':'magic-select',
+		choices = [('0','未出版'),('1','已出版')],
+		widget=widgets.Select(attrs={'class':'selectpicker',
 			'type':'select','id':'status'}),
-	)
+		)
 
-	type = forms.IntegerField(
-		widget = widgets.NumberInput()
+	type = forms.ChoiceField(
+		choices = TypeBook.objects.all().values_list('id','type_book'),
+		widget = widgets.Select(attrs={'class':'selectpicker','placeholder':'类型','id':'type'})
+		)
+	publisher = forms.ChoiceField(
+		choices = Publisher.objects.all().values_list('id','name'),
+		widget = widgets.Select(
+			attrs = {"class": "selectpicker", "data-live-search": "true", "data-width": "100%", "id": "publisher",}
+			)
+		)
+	author = forms.MultipleChoiceField(
+		choices = Author.objects.all().values_list('id','name'),
+		widget = widgets.SelectMultiple(
+			attrs = {"id": "demo-cs-multiselect"}
+			)
 		)
